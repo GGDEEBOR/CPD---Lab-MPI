c3b382d6301d7ecabdb5cf17c7d53ecb4dd3d8fa
EDSON <84099128+GGDEEBOR@users.noreply.github.com>
2024-10-28 11:59:28 -0500

    Add files via upload

---
+++ 3_3v2.cpp
@@ -0,0 +1,41 @@
+#include <mpi.h>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    int process_id, comm_sz;
+    int local_value, received_value;
+
+    // Inicialización de MPI
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &process_id);
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+
+    local_value = process_id;
+    
+    int step = 1;
+    while (step < comm_sz) {
+        if (process_id % (2 * step) == 0) {
+            // Procesos que reciben y suman el valor de sus vecinos
+            int source = process_id + step;
+            if (source < comm_sz) { // Verificar que el proceso vecino existe
+                MPI_Recv(&received_value, 1, MPI_INT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                local_value += received_value;
+            }
+        } else if (process_id % step == 0) {
+            // Procesos que envían su valor local
+            int destination = process_id - step;
+            MPI_Send(&local_value, 1, MPI_INT, destination, 0, MPI_COMM_WORLD);
+            break;
+        }
+        step *= 2;
+    }
+
+    if (process_id == 0) {
+        cout << "Suma global: " << local_value << endl;
+    }
+
+    // Finalización de MPI
+    MPI_Finalize();
+    return 0;
+}
